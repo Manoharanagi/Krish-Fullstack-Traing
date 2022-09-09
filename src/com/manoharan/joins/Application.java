a96485f277f1ae84f83146d6301e90ccbf9e3a91
Manoharan Agi <manoharanagi99@gmail.com>
2022-09-09 22:22:17 +0530

    first commit

---
+++ src/com/manoharan/joins/Application.java
@@ -0,0 +1,16 @@
+package com.manoharan.joins;
+
+
+public class Application {
+    public static void main(String[] args) throws InterruptedException {
+
+        Printer printer = new Printer();
+        Thread thread = new Thread(printer);
+        thread.start();
+        thread.join();
+        for(int i=0;i<100;i++){
+            System.out.println("Main Thread "+i);
+        }
+
+    }
+}
---
+++ src/com/manoharan/yield/Application.java
@@ -0,0 +1,14 @@
+package com.manoharan.yield;
+
+
+public class Application {
+    public static void main(String[] args) {
+
+        Printer printer = new Printer();
+        printer.start();
+        for(int i=0;i<10;i++){
+            System.out.println("Main Thread "+i);
+        }
+
+    }
+}
